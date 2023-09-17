966713c30b0e7950f602963616c130bcdce7c504
Jatin Kshatriya <83948568+Jatin-tec@users.noreply.github.com>
2023-09-17 15:46:09 +0000

    deployment config

--- src/component/Home.js
+++ src/component/Home.js
@@ -53,66 +53,79 @@ export default function Home() {
                 }
         };
 
+        function sleep(ms) {
+                return new Promise(resolve => setTimeout(resolve, ms));
+        }
+
+        const getCrawlResult = async (generatedUrlId) => {
+                const response = await axios({
+                        method: 'post',
+                        url: 'https://api.dataforseo.com/v3/on_page/pages',
+                        auth: {
+                                username: 'urvashikshatriya9@gmail.com',
+                                password: '22cd312c7a13eb93'
+                        },
+                        data: [
+                                { 
+                                        id: generatedUrlId,
+                                        page_url: url
+                                }
+                        ],
+                        headers: {
+                                'content-type': 'application/json'
+                        }
+                })
+                try {
+                        console.log("I am working correctly");
+                        console.log(response.data);
+
+                        const result = response?.data?.tasks[0]?.result[0];
+                        if (result.crawl_progerss)
+                        console.log(result);
+
+                        console.log("I am still  working correctly");
+                        if (result) {
+                                // Update your UI to display the result metrics.
+                                if (result?.crawl_progress === "in_progress") {
+                                        sleep(5000);
+                                        getCrawlResult(generatedUrlId);
+                                }
+                                console.log(result);
+                        } else {
+                                console.error('No result found in the API response.');
+                        }
+                        console.log("I am still  working correctly part2");
+                }
+                catch (error) {
+                        console.log(error);
+                }
+        }
+
         const handleAuditClick = async () => {
                 const generatedUrlId = await generateUrlId();
                 if (generatedUrlId) {
-                        axios({
-                                method: 'post',
-                                url: 'https://api.dataforseo.com/v3/on_page/pages',
-                                auth: {
-                                        username: 'urvashikshatriya9@gmail.com',
-                                        password: '22cd312c7a13eb93'
-                                },
-                                data: [
-                                        {
-                                                id: generatedUrlId,
-                                                page_url: url 
-                                        }
-                                ],
-                                headers: {
-                                        'content-type': 'application/json'
-                                }
-                        })
-                                .then(function (response) {
-                                        console.log("I am working correctly");
-                                        console.log(response.data);
-
-                                        const result = response?.data?.tasks[0].result[0];
-                                        console.log(result);
-                                        console.log("I am still  working correctly");
-                                        if (result) {
-                                                // Update your UI to display the result metrics.
-                                                console.log(result);
-                                        } else {
-                                                console.error('No result found in the API response.');
-                                        }
-                                        console.log("I am still  working correctly part2");
-                                })
-                                .catch(function (error) {
-                                        console.log(error);
-                                });
+                        await sleep(2000);
+                        getCrawlResult(generatedUrlId);
+                
                 } else {
                         // Handle the case where generating the URL ID failed
                         console.error('Failed to generate URL ID. Cannot make API call.');
                 }
         };
 
-
         return (
-                <>
-                        <div>
-                                <h1 className='text-center'>Get A Free SEO Report!</h1>
-                                <div className="container my-3">
-                                        <div className="mb-3">
-                                                <label htmlFor="exampleInputEmail1" className="form-label">Enter Your URL Here:</label>
-                                                <input type="text" className="form-control" id="exampleInputEmail1" placeholder='http://example.com' aria-describedby="emailHelp" value={url} onChange={handleUrlChange} />
-                                        </div>
-                                        <Link to='/other'>
-                                                <button className="btn btn-primary" onClick={handleAuditClick}>Get a free SEO Audit Report</button>
-                                        </Link>
-                                        {error && <div className="alert alert-danger mt-3">{error}</div>}
+                <div>
+                        <h1 className='text-center'>Get A Free SEO Report!</h1>
+                        <div className="container my-3">
+                                <div className="mb-3">
+                                        <label htmlFor="exampleInputEmail1" className="form-label">Enter Your URL Here:</label>
+                                        <input type="text" className="form-control" id="exampleInputEmail1" placeholder='http://example.com' aria-describedby="emailHelp" value={url} onChange={handleUrlChange} />
                                 </div>
+                                <Link to='/other'>
+                                        <button className="btn btn-primary" onClick={handleAuditClick}>Get a free SEO Audit Report</button>
+                                </Link>
+                                {error && <div className="alert alert-danger mt-3">{error}</div>}
                         </div>
-                </>
+                </div>
         );
 }
