4688870c86954f0abd4e327c90793e0d39fd1232
Jatin Kshatriya <83948568+Jatin-tec@users.noreply.github.com>
2023-09-17 16:06:20 +0000

    update config 2

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://Jatin-tec.github.io",
+  base: "https://urvashitech/SEO-detector.github.io",
   plugins: [react()],
 });
